421e76913b98537d766e5bef90876531513e1173
shubhamkislay <shubhamkislay@gmail.com>
2020-03-13 21:48:45 +0530

    splashscreen animation

--- app/src/main/java/com/shubhamkislay/jetpacklogin/SplashScreen.java
+++ app/src/main/java/com/shubhamkislay/jetpacklogin/SplashScreen.java
@@ -1,25 +1,54 @@
 package com.shubhamkislay.jetpacklogin;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private ImageView splash_logo, splash_logo_gradient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash_screen);
 
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
-        Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+
+        splash_logo = findViewById(R.id.splash_logo);
+
+        splash_logo_gradient = findViewById(R.id.splash_logo_gradient);
+
 
        // overridePendingTransition(R.anim.enter_left_to_right, R.anim.exit_left_to_right);
 
-        startActivity(intent);
-        finish();
+
+        final Animation hyperspaceJump = AnimationUtils.loadAnimation(SplashScreen.this, R.anim.image_bounce);
+
+        hyperspaceJump.setRepeatMode(Animation.INFINITE);
+
+        splash_logo_gradient.setAnimation(hyperspaceJump);
+
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
     }
 }
