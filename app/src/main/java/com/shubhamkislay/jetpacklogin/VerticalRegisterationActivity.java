31af5e7fae7ea25b925e17336033d7040158cb9f
shubhamkislay <shubhamkislay@gmail.com>
2020-03-17 15:58:32 +0530

    vertical register activity

--- app/src/main/java/com/shubhamkislay/jetpacklogin/VerticalRegisterationActivity.java
+++ app/src/main/java/com/shubhamkislay/jetpacklogin/VerticalRegisterationActivity.java
@@ -14,6 +14,10 @@ public class VerticalRegisterationActivity extends AppCompatActivity {
 
     FrameLayout frameLayout;
     List<Fragment> fragmentList;
+    RegisterEmailEntryFragment registerEmailEntryFragment;
+    RegisterAuthenticateActivity registerAuthenticateActivity;
+    RegisterUsernameEntryFragment registerUsernameEntryFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,25 @@ public class VerticalRegisterationActivity extends AppCompatActivity {
 
 
         frameLayout = findViewById(R.id.framelayout);
+
+
+
+        registerAuthenticateActivity = new RegisterAuthenticateActivity();
+        registerEmailEntryFragment = new RegisterEmailEntryFragment();
+        registerUsernameEntryFragment = new RegisterUsernameEntryFragment();
         fragmentList = new ArrayList<>();
+
+        fragmentList.add(registerAuthenticateActivity);
+        fragmentList.add(registerEmailEntryFragment);
+        fragmentList.add(registerUsernameEntryFragment);
+
+
+        getSupportFragmentManager().beginTransaction()
+              //  .setCustomAnimations(R.anim.enter_top_to_bottom, R.anim.exit_left_to_right)
+                .replace(R.id.container_layout, registerEmailEntryFragment).commit();
+
+
+
 
 
 
